bbbb96d61e050b1b31194a66f6b9bc4ddb91aa75
Patlund <patrik.ojerssonlundgren@gmail.com>
2022-09-20 19:04:02 +0200

    starting to create the HTML components and starting with the CSS positioning.

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -7,31 +7,44 @@ export default function Booking() {
         </div>
       </div>
       <div className="container">
+
         <div className="content">
+
           <div className="input">
-            <input type="search" placeholder="Till"></input>
-            <input type="search" placeholder="Från"></input>
+            <div className="searchText">
+              <p>Sök Resa</p>
+            </div>
+            <input class="searchfield" type="search" placeholder="Till"></input>
+            <input class="searchfield" type="search" placeholder="Från"></input>
             <input type="date"></input>
             + lägg till återresa
           </div>
-          <div className="ticketamounts">
-            <div className="ticket vuxen">
-              Vuxen
+          <div className="ticketcontainer">
+            <div className="ticketamounts">
+              <div className="ticket vuxen">
+                Vuxen
+              </div>
+              <div className="ticket bu">
+                Barn/Ungdom
+              </div>
+              <div className="ticket student">
+                Student
+              </div>
+              <div className="ticket pensioner">
+                Pensionär
+              </div>
             </div>
-            <div className="ticket bu">
-              Barn/Ungdom
-            </div>
-            <div className="ticket student">
-              Student
-            </div>
-            <div className="ticket pensioner">
-              Pensionär
+            <div className="ticketbuttons">
+              <div className="ticketbutton vuxen"><button className="plusbutton">+</button><p>0</p><button className="minusbutton">-</button></div>
+              <div className="ticketbutton bu"><button className="plusbutton">+</button><p>0</p><button className="minusbutton">-</button></div>
+              <div className="ticketbutton student"><button className="plusbutton">+</button><p>0</p><button className="minusbutton">-</button></div>
+              <div className="ticketbutton pensioner"><button className="plusbutton">+</button><p>0</p><button className="minusbutton">-</button></div>
             </div>
           </div>
         </div>
       </div>
       <div className="button">
-        <button>Sök Resa</button>
+        <button className="srbutton">Sök Resa</button>
       </div>
     </div>
 
