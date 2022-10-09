68e5a89794de8b26e36a9cd14ef626c762363544
CaptainSara <sara.iqbal@hotmail.se>
2022-10-09 22:57:48 +0200

    QR code replaces

--- src/pages/TimeTable.jsx
+++ src/pages/TimeTable.jsx
@@ -1,11 +1,11 @@
-import QrCode from "react-qr-code";
 import { Link } from "react-router-dom";
 export default function TimeTable() {
+
   return (
     <div className="main">
       <div className="navbar">
         <div className="goback">
-          <Link className="goback-link" to="/">X Gå tillbaka</Link>
+          <Link className="goback-link" to="/">X Gå tillbakaa</Link>
         </div>
       </div>
     </div>
